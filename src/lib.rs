28f95ed12eac1dc60edae6567e9c8d09899b038b
Denis Bazhenov <dotsid@gmail.com>
2024-05-18 14:29:57 +0200

    ensure! macros added

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,12 @@ mod page;
 
 pub use memory::{Handle, Memory, Ptr, Storable, Transaction};
 pub use page::Addr;
+
+#[macro_export]
+macro_rules! ensure {
+    ($predicate:expr, $error:expr) => {
+        if !($predicate) {
+            return Err($error);
+        }
+    };
+}
