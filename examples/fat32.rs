c7c123e9e7f30c0404d0c49354333993856eb031
Denis Bazhenov <dotsid@gmail.com>
2024-05-20 21:25:38 +0200

    FAT32 snapshots testing

--- examples/fat32.rs
+++ examples/fat32.rs
@@ -8,35 +8,42 @@ use std::io::{self, Read, Seek, SeekFrom, Write};
 fn main() {
     let page_count = 100;
     let mut pool = PagePool::new(page_count);
-    {
-        // Formatting file system
-        let mut layer = FsLayer::new(&pool, page_count);
-        format_volume(&mut layer, FormatVolumeOptions::new()).unwrap();
-        pool.commit(layer.snapshot);
-    }
 
+    // Formatting file system
     let mut layer = FsLayer::new(&pool, page_count);
-    let fs = FileSystem::new(&mut layer, FsOptions::new()).unwrap();
-    let root_dir = fs.root_dir();
+    format_volume(&mut layer, FormatVolumeOptions::new()).unwrap();
+    pool.commit(layer.snapshot);
+
     let file_name = "hello.txt";
-    let content = b"Hello World!";
-    {
+    let mut snapshots = vec![];
+
+    // Writing 10 snapshots of FS with different contents of a file
+    for i in 0..10 {
+        let mut layer = FsLayer::new(&pool, page_count);
+        let fs = FileSystem::new(&mut layer, FsOptions::new()).unwrap();
         // Writing file
-        root_dir
+        fs.root_dir()
             .create_file(file_name)
             .unwrap()
-            .write_all(content)
+            .write_all(format!("Hello world {}!", i).as_bytes())
             .unwrap();
+        fs.unmount().unwrap();
+        pool.commit(layer.snapshot);
+        snapshots.push(pool.snapshot());
     }
-    {
+
+    // Making sure all of the snapshots are accessible independently
+    for (i, snapshot) in snapshots.into_iter().enumerate() {
+        let mut layer = FsLayer::from(snapshot, page_count);
         // Reading file
+        let fs = FileSystem::new(&mut layer, FsOptions::new()).unwrap();
         let mut buf = vec![];
-        root_dir
+        fs.root_dir()
             .open_file(file_name)
             .unwrap()
             .read_to_end(&mut buf)
             .unwrap();
-        assert_eq!(content.as_ref(), buf);
+        assert_eq!(format!("Hello world {}!", i).as_bytes(), buf);
     }
 }
 
@@ -49,6 +56,14 @@ struct FsLayer {
 impl FsLayer {
     fn new(pool: &PagePool, page_count: usize) -> Self {
         let snapshot = pool.snapshot();
+        Self {
+            snapshot,
+            addr: 0,
+            size: page_count * PAGE_SIZE,
+        }
+    }
+
+    fn from(snapshot: Snapshot, page_count: usize) -> Self {
         Self {
             snapshot,
             addr: 0,
