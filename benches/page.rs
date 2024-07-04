6607309f3f49bfcf7019539392e4bce8b084e419
Denis Bazhenov <dotsid@gmail.com>
2024-07-04 11:08:13 +0200

    arbitrary read/write benchmarks

--- benches/page.rs
+++ benches/page.rs
@@ -3,30 +3,55 @@ use pmem::{
     Addr,
 };
 use rand::{rngs::SmallRng, Rng, SeedableRng};
-use std::hint::black_box;
-use tango_bench::{benchmark_fn, tango_benchmarks, tango_main, Bencher, IntoBenchmarks, Sampler};
+use std::{hint::black_box, ops::Range};
+use tango_bench::{
+    benchmark_fn, tango_benchmarks, tango_main, Bencher, IntoBenchmarks, MeasurementSettings,
+    Sampler,
+};
 
-const DB_SIZE: usize = 10 * 1024;
+const DB_SIZE: usize = 1024 * 1024;
 
 fn page_benchmarks() -> impl IntoBenchmarks {
-    [benchmark_fn("arbitrary_write", bench_arbitrary_write)]
+    [
+        benchmark_fn("arbitrary_read", bench_arbitrary_read),
+        benchmark_fn("arbitrary_write", bench_arbitrary_write),
+    ]
 }
 
-fn bench_arbitrary_write(b: Bencher) -> Box<dyn Sampler> {
+fn bench_arbitrary_read(b: Bencher) -> Box<dyn Sampler> {
     let mut rng = SmallRng::seed_from_u64(b.seed);
     let mem = generate_mem(&mut rng);
     b.iter(move || {
-        let len = rng.gen_range(1..1024);
-        let addr = rng.gen_range(0..DB_SIZE - len);
-
+        let (addr, len) = random_segment(&mut rng, 0..DB_SIZE);
         let snapshot = mem.snapshot();
         let _ = black_box(snapshot.read(addr as Addr, len)).unwrap();
     })
 }
 
+fn bench_arbitrary_write(b: Bencher) -> Box<dyn Sampler> {
+    let mut rng = SmallRng::seed_from_u64(b.seed);
+
+    let mut buffer = [0u8; DB_SIZE];
+    rng.fill(&mut buffer[..]);
+
+    let mem = PagePool::new(DB_SIZE / PAGE_SIZE + 1);
+    let mut snapshot = mem.snapshot();
+    b.iter(move || {
+        let (addr, len) = random_segment(&mut rng, 0..DB_SIZE);
+        snapshot.write(addr as Addr, &buffer[..len]);
+    })
+}
+
+fn random_segment(rng: &mut SmallRng, mut range: Range<usize>) -> (usize, usize) {
+    let len = rng.gen_range(1..1024);
+    range.end -= len;
+    let addr = rng.gen_range(range);
+    (addr, len)
+}
+
 fn generate_mem(rng: &mut SmallRng) -> PagePool {
     const SNAPSHOTS: usize = 100;
-    const PATCHES: usize = 10;
+    const PATCHES: usize = 1000;
 
     let mut buffer = [0u8; DB_SIZE];
     rng.fill(&mut buffer[..]);
@@ -35,8 +60,7 @@ fn generate_mem(rng: &mut SmallRng) -> PagePool {
     for _ in 0..SNAPSHOTS {
         let mut snapshot = mem.snapshot();
         for _ in 0..PATCHES {
-            let len = rng.gen_range(1..DB_SIZE);
-            let addr = rng.gen_range(0..DB_SIZE - len);
+            let (addr, len) = random_segment(rng, 0..DB_SIZE);
             snapshot.write(addr as Addr, &buffer[..len]);
         }
         mem.commit(snapshot);
@@ -45,4 +69,7 @@ fn generate_mem(rng: &mut SmallRng) -> PagePool {
 }
 
 tango_benchmarks!(page_benchmarks());
-tango_main!();
+tango_main!(MeasurementSettings {
+    samples_per_haystack: 1000,
+    ..Default::default()
+});
