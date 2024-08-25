1e97500d4b67479b8caab3ebbb0dc608fe423e6d
Denis Bazhenov <dotsid@gmail.com>
2024-08-25 22:20:12 +0200

    Working on FsDriver

--- pmem/src/driver.rs
+++ pmem/src/driver.rs
@@ -1,5 +1,10 @@
 use crate::page::{PageNo, PAGE_SIZE};
-use std::{collections::BTreeMap, io};
+use std::{
+    collections::BTreeMap,
+    fs,
+    io::{self, Read, Seek, SeekFrom, Write},
+    path::Path,
+};
 
 pub trait PageDriver: Send {
     fn read_page(&mut self, page_no: PageNo) -> io::Result<Option<&[u8; PAGE_SIZE]>>;
@@ -26,6 +31,74 @@ impl PageDriver for MemoryDriver {
     }
 
     fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub struct FileDriver {
+    pages: BTreeMap<PageNo, Box<[u8; PAGE_SIZE]>>,
+    file: std::fs::File,
+}
+
+impl FileDriver {
+    pub fn new(file: impl AsRef<Path>) -> io::Result<Self> {
+        let file = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(false)
+            .open(file)?;
+        let pages = BTreeMap::new();
+        Ok(Self { file, pages })
+    }
+
+    fn ensure_loaded(&mut self, page_no: PageNo) -> io::Result<()> {
+        if !self.pages.contains_key(&page_no) {
+            return Ok(());
+        }
+        let offset = (page_no as u64) * (PAGE_SIZE as u64);
+        self.file.seek(SeekFrom::Start(offset))?;
+        let mut page = Box::new([0; PAGE_SIZE]);
+        self.file.read_exact(page.as_mut())?;
+        self.pages.insert(page_no, page);
+        Ok(())
+    }
+}
+
+impl PageDriver for FileDriver {
+    fn read_page(&mut self, page_no: PageNo) -> io::Result<Option<&[u8; PAGE_SIZE]>> {
+        self.ensure_loaded(page_no)?;
+        Ok(self.pages.get(&page_no).map(|p| p.as_ref()))
+    }
+
+    fn read_page_mut(&mut self, page_no: PageNo) -> io::Result<&mut [u8; PAGE_SIZE]> {
+        self.ensure_loaded(page_no)?;
+        Ok(self
+            .pages
+            .entry(page_no)
+            .or_insert_with(|| Box::new([0; PAGE_SIZE])))
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.file.flush()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_read_and_write_page() -> io::Result<()> {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let temp_file = temp_dir.path().join("test.db");
+        let mut driver = FileDriver::new(temp_file)?;
+        let page_no = 0;
+        let page = driver.read_page_mut(page_no)?;
+        page[0] = 42;
+        driver.flush()?;
+        let page = driver.read_page(page_no)?.unwrap();
+        assert_eq!(page[0], 42);
         Ok(())
     }
 }
